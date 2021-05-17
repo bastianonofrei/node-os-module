9caff900b536a5d3960f385fa911c63c48bab335
unknown <sonofra98@gmail.com>
2021-05-18 02:35:21 +0300

    file system module added

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ let totalMemo = os.totalmem();
 let freeMemo = os.freemem();
 
 console.log(`Total memory ${totalMemo} ; Free memory ${freeMemo}`);
+
+
+const fs = require('fs')
+
+let files = fs.readdirSync('./')
+
+console.log(`files : ${files}`)
