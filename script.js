914408fda73baf55407f9b57639bebe436579b2c
unknown <sonofra98@gmail.com>
2021-05-18 16:34:36 +0300

    created a small local server listener

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ const path = require('path');
 let totalMemo = os.totalmem();
 let freeMemo = os.freemem();
 
+const http = require('http');
+const server = http.createServer((req, res) => {
+    if (req.url === '/')
+        res.write("Hello World");
+    res.end()
+});
+
+server.listen(3000);
+
 console.log(`Total memory ${totalMemo} ; Free memory ${freeMemo}`);
 
 
